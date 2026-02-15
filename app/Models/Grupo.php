edaea9535153304cf83ad57db9eb263badd7a88a
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-15 19:38:01 -0300

    atualização: adiciona belongto para grupo

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Grupo extends Model
 {
     use HasFactory;
-    //
+
+    protected $casts = [
+        'horario' => 'datetime:H:i',
+    ];
+
+    function projeto(): BelongsTo
+    {
+        return $this->belongsTo(Projeto::class);
+    }
 }
