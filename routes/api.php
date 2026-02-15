83dadc82cd6a5ee48817d2ae5c2609a13d04e4ee
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-15 14:07:43 -0300

    atualização: adiciona prefixo v1 rotas api, adiciona tratamento caso não encontre o modelo , cria CRUD inicial para projeto, grupo, associado, agenda

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,45 @@ use App\Http\Controllers\Api\ProjetoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::prefix('associados')
+        ->missing(function (Request $request) {
+            return response()->json([
+                'message' => 'Associado não encontrado.'
+            ], 404);
+        })
+        ->group(function () {
+            Route::resource("", AssociadoController::class)->parameters(['' => 'associado']);
+            Route::patch("{associado}/activate", [AssociadoController::class, 'activate']);
+            Route::patch("{associado}/deactivate", [AssociadoController::class, 'deactivate']);
+        });
 
-Route::get("/agendas", [AgendaController::class, 'index']);
-Route::get("/grupos", [GrupoController::class, 'index']);
-Route::get("/projetos", [ProjetoController::class, 'index']);
+    Route::prefix('projetos')
+        ->missing(function (Request $request) {
+            return response()->json([
+                'message' => 'projeto não encontrado.'
+            ], 404);
+        })
+        ->group(function () {
+            Route::resource("", ProjetoController::class)->parameters(['' => 'projeto']);
+        });
+    Route::prefix('grupos')
+        ->missing(function (Request $request) {
+            return response()->json([
+                'message' => 'grupo não encontrado.'
+            ], 404);
+        })
+        ->group(function () {
+            Route::resource("", GrupoController::class)->parameters(['' => 'grupo']);
+        });
+    Route::prefix('agendas')
+        ->missing(function (Request $request) {
+            return response()->json([
+                'message' => 'agenda não encontrado.'
+            ], 404);
+        })
+        ->group(function () {
 
-Route::resource("associados", AssociadoController::class);
-Route::resource("grupos", GrupoController::class);
+            Route::resource("", AgendaController::class)->parameters(['' => 'agenda']);
+        });
+});
