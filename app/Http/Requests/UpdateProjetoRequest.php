9a4025c06fa4369d5d8a9f6806f30ceacaea7659
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-15 21:44:07 -0300

    adiciona: fillable, store e update request para o modelo projeto

--- app/Http/Requests/UpdateProjetoRequest.php
+++ app/Http/Requests/UpdateProjetoRequest.php
@@ -11,7 +11,7 @@ class UpdateProjetoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateProjetoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nome'   => ['sometimes', 'string', 'max:255'],
+            'regiao' => ['sometimes', 'string', 'max:100'],
+            'valor'  => ['sometimes', 'numeric', 'min:0'],
         ];
     }
 }
--- app/Models/Projeto.php
+++ app/Models/Projeto.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Projeto extends Model
 {
     use HasFactory;
-    //
+
+    protected $fillable = [
+        'nome',
+        'regiao',
+        'valor',
+        "status",
+        "concluido",
+    ];
+
+    function grupos(): hasMany
+    {
+        return $this->hasMany(Grupo::class);
+    }
 }
