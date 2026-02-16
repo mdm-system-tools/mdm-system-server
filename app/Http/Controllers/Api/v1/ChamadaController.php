68951b6e1675546427fb35bc9e6244ce2034156a
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-16 16:53:23 -0300

    implementação: crud basico para chamada

--- app/Http/Controllers/Api/v1/ChamadaController.php
+++ app/Http/Controllers/Api/v1/ChamadaController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreChamadaRequest;
 use App\Http\Requests\UpdateChamadaRequest;
+use App\Http\Resources\ChamadaResource;
+use App\Models\Associado;
 use App\Models\Chamada;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ChamadaController extends Controller
 {
@@ -14,15 +17,7 @@ class ChamadaController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return ChamadaResource::collection(Chamada::all());
     }
 
     /**
@@ -30,7 +25,23 @@ class ChamadaController extends Controller
      */
     public function store(StoreChamadaRequest $request)
     {
-        //
+        try {
+            if (empty($request->validated())) {
+                return response()->json(["message" => "dados invalidos"], ResponseAlias::HTTP_BAD_REQUEST);
+            }
+
+            if ($request->validated()["representante"]) {
+                if (Associado::findOrFail($request->validated()["numero_inscricao"])->representante()["id"] == null) {
+                    return response()->json(["message" => "este associado não possui um representante, operação cancelada"], ResponseAlias::HTTP_BAD_REQUEST);
+                }
+            }
+
+            if (Chamada::create($request->validated())) {
+                return response()->json(["message" => "Chamada cadastrada com sucesso!"], ResponseAlias::HTTP_CREATED);
+            }
+        } catch (\Exception $e) {
+            return response()->json(["message" => "Erro ao cadastrar chamada!", "error" => $e->getMessage()], ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -38,15 +49,7 @@ class ChamadaController extends Controller
      */
     public function show(Chamada $chamada)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Chamada $chamada)
-    {
-        //
+        return new ChamadaResource($chamada);
     }
 
     /**
@@ -54,7 +57,13 @@ class ChamadaController extends Controller
      */
     public function update(UpdateChamadaRequest $request, Chamada $chamada)
     {
-        //
+        try {
+            if ($chamada->update($request->validated())) {
+                return response()->json(["message" => "Chamada atualizado com sucesso!"], ResponseAlias::HTTP_OK);
+            }
+        } catch (\Exception $e) {
+            return response()->json(["message" => "Erro ao atualizar chamada!", "error" => $e->getMessage()], ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -62,6 +71,12 @@ class ChamadaController extends Controller
      */
     public function destroy(Chamada $chamada)
     {
-        //
+        try {
+            if ($chamada->delete()) {
+                return response()->json(["message" => "chamada apagada com sucesso"], ResponseAlias::HTTP_OK);
+            }
+        } catch (\Exception $e) {
+            return response()->json(["message" => "ocorreu um erro", "error" => $e->getMessage()], ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
