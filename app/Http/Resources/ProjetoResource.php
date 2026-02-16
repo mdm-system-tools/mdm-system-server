9f38e03afc6ca35e1845f7fc6c7ae1c4aed3413c
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-15 21:44:31 -0300

    adiciona: resource para o modelo projeto

---
+++ app/Http/Resources/ProjetoResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjetoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "grupos" =>$this->grupos->isNotEmpty() ? [
+                "horario" => $this->grupos->map(function ($grupo) {
+                    return Carbon::parse($grupo->horario)->format('H:i');
+                }),
+            ] : null,
+            "nome" => $this->nome,
+            "regiao" => $this->regiao,
+            'valor' => number_format($this->valor, "2", ",", "."),
+            'status' => $this->status ? "ativo" : "inativo",
+            "concluido" => $this->concluido ? "sim" : "nao",
+        ];
+    }
+}
