bdab2f462741157c2d6b3d3ab5778e67793c0058
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-16 14:15:48 -0300

    adiciona novas factorys ao DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,13 @@
 namespace Database\Seeders;
 
 use App\Models\Associado;
+use App\Models\Chamada;
 use App\Models\Grupo;
+use App\Models\Local;
 use App\Models\Projeto;
+use App\Models\Reuniao;
 use Database\Factories\AssociadoFactory;
+use Database\Factories\LocalFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,7 +23,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Projeto::factory()->count(5)->create();
-        Grupo::factory()->count(5)->create();
+        Grupo::factory()->count(20)->create();
         Associado::factory()->count(10)->create();
+        Local::factory()->count(10)->create();
+        Reuniao::factory()->count(10)->create();
+        Chamada::factory()->count(10)->create();
     }
 }
