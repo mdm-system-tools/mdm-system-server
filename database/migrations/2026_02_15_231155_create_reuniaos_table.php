2b8ff252e99fe8f52feb6cee232313c6a02e3dac
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-15 21:36:37 -0300

    removação: modelo agenda e anexo

--- database/migrations/2026_02_15_231155_create_reuniaos_table.php
+++ database/migrations/2026_02_15_231155_create_reuniaos_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId("projeto_id");
             $table->foreignId("chamada_id");
-            $table->date("data");
-
+            $table->date("data_reuniao");
             $table->timestamps();
         });
     }
