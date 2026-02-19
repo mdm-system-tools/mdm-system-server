ce3fbf7f7644021c02613ccd975f06ffe0421063
Eduardo Silva <eduardofox989@protonmail.com>
2026-02-19 18:33:05 -0300

    atualização: todos os campos de associado Resource agora são snake_case

--- app/Http/Resources/AssociadoResource.php
+++ app/Http/Resources/AssociadoResource.php
@@ -16,17 +16,17 @@ class AssociadoResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "numeroInscricao" => Formatador::formatNumInscricao($this->numero_inscricao),
+            "numero_inscricao" => Formatador::formatNumInscricao($this->numero_inscricao),
             "nome"            => $this->nome,
-            "estadoCivil"     => $this->estado_civil,
-            "dataNascimento"  => Formatador::formatDateToDayMonthYear($this->data_nascimento),
+            "estado_civil"     => $this->estado_civil,
+            "data_nascimento"  => Formatador::formatDateToDayMonthYear($this->data_nascimento),
             "email"           => $this->email,
             "cpf"             => Formatador::formatCPF($this->CPF),
             "rg"              => Formatador::formatRG($this->RG),
             "nis"             => Formatador::formatNis($this->NIS),
             "cras"            => Formatador::formatCras($this->cras),
-            "rendaFamiliar"   => Formatador::formatValueBR($this->renda_familiar),
-            "dataInscricao"   => Formatador::formatDateToDayMonthYear($this->data_inscricao),
+            "renda_familiar"   => Formatador::formatValueBR($this->renda_familiar),
+            "data_inscricao"   => Formatador::formatDateToDayMonthYear($this->data_inscricao),
             "status"          => $this->status ? "Ativo" : "Inativo",
             "dependente"      => $this->dependente ? [
                 "id"   => $this->dependente->id,
@@ -35,8 +35,8 @@ class AssociadoResource extends JsonResource
             "grupo"           => $this->grupo ? [
                 "horario" => Formatador::formatDateToHoursMinutes($this->grupo->horario),
             ] : null,
-            "documentoImg"    => $this->documento_img,
-            "certidaoImg"     => $this->certidao_img,
+            "documento_img"    => $this->documento_img,
+            "certidao_img"     => $this->certidao_img,
             "representante"   => $this->representante ? [
                 "nome" => $this->representante->nome,
                 "cpf"  => $this->representante->CPF,
